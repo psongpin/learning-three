03a626d35c61087f4eedff9bdf09ce1e3c76bfbb
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2022-02-21 11:44:13 +0800

    feat: basic animation in game loop

--- pages/basic-scene.tsx
+++ pages/basic-scene.tsx
@@ -1,13 +1,27 @@
+import { useRef } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
 import { PerspectiveCamera } from '@react-three/drei'
+import { Mesh } from 'three'
 
-const Box: React.FC = () => {
+type BoxProps = MeshProps & {
+  color: string
+}
+
+const Box: React.FC<BoxProps> = ({ color, ...props }) => {
+  const meshRef = useRef<Mesh>()
+
+  useFrame(({ clock }) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x = clock.getElapsedTime()
+    }
+  })
+
   return (
-    <mesh>
+    <mesh {...props} ref={meshRef}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color="#ff0000" />
+      <meshBasicMaterial color={color} />
     </mesh>
   )
 }
@@ -22,13 +36,9 @@ const BasicScene: NextPage = () => {
 
       <div style={{ height: '100vh' }}>
         <Canvas>
-          <PerspectiveCamera
-            makeDefault
-            fov={75}
-            aspect={800 / 600}
-            position={[0, 0, 3]}
-          />
-          <Box />
+          <PerspectiveCamera makeDefault fov={75} position={[0, 0, 3]} />
+
+          <Box color="red" position={[0, 0, 0]} />
         </Canvas>
       </div>
     </>
